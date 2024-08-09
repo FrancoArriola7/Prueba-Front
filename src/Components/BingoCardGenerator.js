082cdfc1ebea82f025c0f70e658674647716f82b
FrancoArriola7 <113956731+FrancoArriola7@users.noreply.github.com>
2024-08-09 00:46:41 -0300

    Update BingoCardGenerator.js api url

--- src/Components/BingoCardGenerator.js
+++ src/Components/BingoCardGenerator.js
@@ -19,7 +19,7 @@ const BingoCardGenerator = () => {
         }
 
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/cartons/generar/', {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/cartons/generar/`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
